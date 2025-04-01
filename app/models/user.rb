491d9cbfcfa9dc7febfc35d0fd7310fbe34fa1c7
hwangisavailable <hoang.dangviet@pixta.co.jp>
2025-04-01 16:05:42 +0700

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -18,4 +20,18 @@ class User < ApplicationRecord
       BCrypt::Password.create(string, cost: cost)
     end
   end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(self.remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
 end
