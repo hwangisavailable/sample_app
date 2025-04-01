829b31f6eacd9b51550ad2c3b3d489b43518fd47
hwangisavailable <hoang.dangviet@pixta.co.jp>
2025-04-01 16:33:26 +0700

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,9 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
+
+    # Guard against session replay attacks
+    session[:session_token] = user.session_token
   end
 
   def remember(user)
@@ -11,7 +14,10 @@ module SessionsHelper
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -31,13 +37,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil
   end
 
-  # Forgets a user.
-  def forget
-    update_attribute(:remember_digest, nil)
-  end
 end
