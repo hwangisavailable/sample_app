042d71743a1a237c2be756605d003d5d35acc5eb
hwangisavailable <hoang.dangviet@pixta.co.jp>
2025-04-02 10:49:22 +0700

    update settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :name,  presence: true, length: { maximum: Settings.user_name_length_max }
+  validates :email, presence: true, length: { maximum: Settings.user_email_length_max },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: Settings.user_password_length_min }
 
   has_secure_password
 end
